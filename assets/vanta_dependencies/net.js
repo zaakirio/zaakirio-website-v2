a42beefb5016568954967b375fff34ed4f51b491
unknown <rubinkazanmusic@gmail.com>
2022-03-26 13:24:19 +0000

    Experimenting with fonts

---
+++ assets/vanta_dependencies/net.js
@@ -0,0 +1,15 @@
+        //Net style from vanta
+        VANTA.NET({
+            el: '#netElement',
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 2000.00,
+            minWidth: 1000.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            backgroundColor: 0x140a25,
+            points: 14.00,
+            maxDistance: 25.00,
+            spacing: 13.00
+        })
